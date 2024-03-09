f47844b9fd9a28b61e56ffa8f517f438f224c52e
Mike Chirkov <mike_chirkov@mail.ru>
2024-03-10 00:02:41 +0500

    spring-security-method

--- src/main/java/ru/netology/springhibernate/config/SecurityConfig.java
+++ src/main/java/ru/netology/springhibernate/config/SecurityConfig.java
@@ -3,21 +3,54 @@ package ru.netology.springhibernate.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/persons/all").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails reader = User.builder()
+                .username("reader")
+                .password(passwordEncoder().encode("reader"))
+                .roles("READ")
+                .build();
+        UserDetails writer = User.builder()
+                .username("writer")
+                .password(passwordEncoder().encode("writer"))
+                .roles("WRITE")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("READ", "WRITE", "DELETE")
+                .build();
+        return new InMemoryUserDetailsManager(admin, reader, writer);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- src/main/java/ru/netology/springhibernate/controller/PersonController.java
+++ src/main/java/ru/netology/springhibernate/controller/PersonController.java
@@ -1,6 +1,9 @@
 package ru.netology.springhibernate.controller;
 
+import jakarta.annotation.security.RolesAllowed;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,21 +22,25 @@ public class PersonController {
     private PersonRepository personRepository;
 
     @GetMapping("/by-city")
+    @Secured("ROLE_READ")
     public List<Person> findByCityOfLiving(@RequestParam String city) {
         return personRepository.findByCityOfLiving(city);
     }
 
     @GetMapping("/by-age")
+    @RolesAllowed("ROLE_WRITE")
     public List<Person> findByAgeLessThanOrderByAge(@RequestParam int age) {
         return personRepository.findByAgeLessThanOrderByAge(age);
     }
 
     @GetMapping("/by-name-and-surname")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public Optional<Person> findByNameAndSurname(@RequestParam String name, @RequestParam String surname) {
         return personRepository.findByNameAndSurname(name, surname);
     }
 
     @GetMapping("/all")
+    @PreAuthorize("authentication.principal.username == 'admin'")
     public List<Person> findAll() {
         return personRepository.findAll();
     }
