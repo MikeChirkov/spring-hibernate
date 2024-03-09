8fe8dac2bc509d178e44b902ba74eab5d9f4b6c5
Mike Chirkov <mike_chirkov@mail.ru>
2024-03-09 19:56:41 +0500

    spring-security

---
+++ src/main/java/ru/netology/springhibernate/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.springhibernate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/persons/all").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
